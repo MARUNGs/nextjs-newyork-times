1728b178860de77fb822a9de75ab75ee315cf1fa
MARUNGs <mi1764@naver.com>
2024-11-15 14:36:51 +0900

    about 반응형

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <>
-      <div className="shadow-lg w-3/5 h-40 ml-96 *:ml-5 ">
+      <div className="shadow-lg w-3/5 h-40 m-auto *:ml-5 ">
         <h1 className="mt-10 mb-6">ABOUT US</h1>
         <p className="text-lg">
           WELCOME TO THE OFFICIAL EXPLORER FOR THE NEW YORK TIMES BEST SELLER
